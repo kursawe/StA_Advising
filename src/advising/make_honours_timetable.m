1ba244336555f6756dc1eb04d1e727d2e01022da
David Rees Jones <7170455+davidreesjones@users.noreply.github.com>
2026-03-13 12:54:44 +0000

    MT5590 doesn't have lectures so remove from timetable tabs

--- src/advising/make_honours_timetable.m
+++ src/advising/make_honours_timetable.m
@@ -5,6 +5,7 @@ arguments
     year string = '2026_27' % or 2027_28 % may need adjustments year to year
 end
 
+M=timetable_clarifications(M);
 year=year{1};
 if mod(str2double(year(1:4)),2)==0
     ind=find((M.Semester==semester)&((M.RunningStatus=="Runs every year")|(M.RunningStatus=="Alternates (2026/27)")));
@@ -51,6 +52,13 @@ end
             & M_test.Timetable~="11am Mon (even weeks), Tue, Thu" ...
             & M_test.Timetable~="12noon Mon (even weeks), Tue, Thu");
         T=M_test(ind_test,[5,1,2]);
+    end
+
+    function M=timetable_clarifications(M)
+        ind_ISM=M.ModuleCode=="MT5590";
+        if sum(ind_ISM)>=1
+            M=M(~ind_ISM,:);
+        end      
     end
 
 
