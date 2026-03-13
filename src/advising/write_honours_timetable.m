99d5a2da82c11ce455d47bfb69122e3147748783
David Rees Jones <7170455+davidreesjones@users.noreply.github.com>
2026-03-13 12:32:31 +0000

    Added output filename option and also added a student-readable version of the catalogue to the excel output file

--- src/advising/write_honours_timetable.m
+++ src/advising/write_honours_timetable.m
@@ -1,14 +1,18 @@
-function write_honours_timetable(file_1,year)
+function write_honours_timetable(file_1,year,output_file)
 %write_honours_timetable 
 %   Given the module catalogue file and starting year, make timetables for
 %   the next three academic years and export to excel
 arguments
     file_1 string = 'Module_catalogue.xlsx' 
     year string = '2026_27' % or 2027_28 % may need adjustments year to year
+    output_file string = 'output-timetable.xlsx' 
 end
+
 year=year{1};
 
 M = load_MC_honours(file_1);
+M.Properties.VariableNames{9}='Division';
+writetable(M(:,[1,2,4,10,9,5:8]),output_file,'Sheet','Catalogue')
 
 semester='S1'; make_timetable_instance(M,semester,year);
 semester='S2'; make_timetable_instance(M,semester,year);
@@ -23,9 +27,9 @@ semester='S2'; make_timetable_instance(M,semester,year);
 
     function make_timetable_instance(M,semester,year)
         [T_odd,T_even,T_other]= make_honours_timetable(M,semester,year);
-        writetable(T_odd,'output-timetable.xlsx','Sheet',strcat(semester,'_',year),'Range','A1')
-        writetable(T_even,'output-timetable.xlsx','Sheet',strcat(semester,'_',year),'Range','E1')
-        writetable(T_other,'output-timetable.xlsx','Sheet',strcat(semester,'_',year),'Range','I1')
+        writetable(T_odd,output_file,'Sheet',strcat(semester,'_',year),'Range','A1')
+        writetable(T_even,output_file,'Sheet',strcat(semester,'_',year),'Range','E1')
+        writetable(T_other,output_file,'Sheet',strcat(semester,'_',year),'Range','I1')
     end
 
 
